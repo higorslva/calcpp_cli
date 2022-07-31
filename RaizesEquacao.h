7d08b93dd431a5eaf437ce4f1210c8452813308f
Higor Silva <higors88@gmail.com>
2022-07-31 12:08:18 -0300

    Add .h header

---
+++ RaizesEquacao.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+void RaizesEquacao(){
+    float a, b, c;
+    std::cout << "Calcular raízes de equações 'ax²+bx+c=0'\n";
+    std::cout << "(Caso o resultado mostre 'nan', as raízes não existem para essa função)" << std::endl;
+    std::cout << "Insira o valor de a: ";
+    std::cin >> a;
+    std::cout << "Insira o valor de b: ";
+    std::cin >> b;
+    std::cout << "Insira o valor de c: ";
+    std::cin >> c;
+}
