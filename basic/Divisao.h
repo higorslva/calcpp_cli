0c18a2ed7a69b59f6920f6ce2eff9dd322b3ae61
Higor Silva <higors88@gmail.com>
2022-08-01 14:54:03 -0300

    Cleaning code

--- basic/Divisao.h
+++ basic/Divisao.h
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <math.h>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
+
 void Divisao(){
   float a, b;
-  std::cout << "Insira o primeiro valor: ";
-  std::cin >> a;
-  std::cout << "Insira o segundo valor: ";
-  std::cin >> b;
-  std::cout << a << " / " << b << " = " << a / b << std::endl;
+  cout << "Insira o primeiro valor: ";
+  cin >> a;
+  cout << "Insira o segundo valor: ";
+  cin >> b;
+  cout << a << " / " << b << " = " << a / b << endl;
 
 }
--- calc.cpp
+++ calc.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <math.h>
+#include <cmath>
 #include <locale>
-#include <stdlib.h>
-
+#include <cstdlib>
+// for basics operations
 #include "./basic/Adicao.h"
 #include "./basic/Subtracao.h"
 #include "./basic/Multiplicacao.h"
 #include "./basic/Divisao.h"
-
-#include "./algebra/RaizesEquacao.h"
-#include "./algebra/RaizQuadrada.h"
-#include "./algebra/RaizBaseX.h"
-#include "./algebra/Potencia.h"
-
+// for algebra
+#include "./algebra/algebra.h"
+// for calculus
 #include "./calculus/Derivada.h"
+#include "./calculus/Integrais.h"
 
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main ()
 {
@@ -22,14 +22,14 @@ int main ()
         setlocale (LC_ALL, "Portuguese");
         int op;
         float a, b, c;
-        std::cout << "Calculadora v0.5\n" << std::endl;
-        std::cout << "Operações possíveis:\n\n";
-        std::cout << "[1] - Soma | [2] - Subtração | [3] - Multiplição\n";
-        std::cout << "[4] - Divisão | [5] - Raiz quadrada | [6] - Raiz com base x\n";
-        std::cout << "[7] - Potência | [8] - Raízes de uma função do segundo grau | [9] - Derivada\n";
-        std::cout << "[10] - Sair\n\n";
-        std::cout << "Insira o número da operação que deseja: ";
-        std::cin >> op;
+        cout << "Calculadora v0.5.15\n" << endl;
+        cout << "Operações possíveis:\n\n";
+        cout << "[1] - Soma | [2] - Subtração | [3] - Multiplição\n";
+        cout << "[4] - Divisão | [5] - Cálculos Algébricos | [6] - Raízes de uma função do segundo grau\n";
+        cout << "[7] - Derivada | [8] - Integrais\n";
+        cout << "[0] - Sair\n\n";
+        cout << "Insira o número da operação que deseja: ";
+        cin >> op;
     #ifdef _WIN32
         system("cls");
     #else
@@ -44,25 +44,24 @@ int main ()
         }else if (op == 4){
             Divisao();
         }else if (op == 5){
-            RaizQuadrada();
+            Algebra();
         }else if (op == 6){
-            RaizBaseX();
-        }else if (op == 7){
-            Potencia();
-        }else if(op == 8){
             RaizesEquacao();
-        }else if (op == 9){
+        }else if (op == 7){
             Derivada();
-        }else if(op == 10){
-          return 0;
+        }else if (op ==8){
+            Integrais();
+        }
+        else if(op == 0){
+          break;
         }
         int continuar;
-        std::cout << "Deseja fazer outra operacao? \n [1] - Sim \n [2] - Não" << std::endl;
-        std::cin >> continuar;
+        cout << "Deseja fazer outra operacao? \n [1] - Sim \n [2] - Não" << endl;
+        cin >> continuar;
         if(continuar == 2){
             break;
         }else if (continuar != 1){
-            std::cout <<  "Comando Invalido. Saindo..." << std::endl;
+            cout <<  "Comando Invalido. Saindo..." << endl;
             break;
         }
         #ifdef _WIN32
